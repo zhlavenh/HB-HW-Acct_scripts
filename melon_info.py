9539f2acb742dfc8a74139e2909131d957bb0fae
Zee Lavenhouse <Zeehlavenhouse@gmail.com>
2023-03-06 18:42:54 -0500

    working on input of new melons

--- melon_info.py
+++ melon_info.py
@@ -1,18 +1,29 @@
 """Print out all the melons in our inventory."""
+#Importing information
+from melons import melons, melon_names
+
+#Requesting user input
+more_melons = input("Would you like to add for melons Y/N: ")
+
+if more_melons =="Y" or more_melons == "y":
+
+    new_melon = input("insert the info as name, price, seedless(True/False), flesh_color, rind_color, avg_weight: ")
+    new_melon = new_melon.lstrip().split(",")
+
+    melon_names[len(melon_names)+1] = new_melon[0]
+
+    melons[new_melon[0]] = {
+        "prices": new_melon[1],
+        "seedless": new_melon[2],
+        "flesh_color": new_melon[3],
+        "rind_color": new_melon[4],
+        "avg_weight": new_melon[5]}
 
 
-from melons import melon_names, melon_prices, melon_seedlessness 
+print(melons)
 
-
-def print_melon(name, price, seedless):
-    """Print each melon with corresponding attribute information."""
-
-    have_or_have_not = 'have'
-    if seedless:
-        have_or_have_not = 'do not have'
-
-    print(f'{name}s {have_or_have_not} seeds and are ${price}')
-
-
-for i in melon_names:
-    print_melon(melon_names[i], melon_prices[i], melon_seedlessness[i])
+#print report for all melons
+# for i in melon_names:
+#     name = melon_names[i]
+#     melon_info = melons[name]
+#     print(f'{name}\nprice: {melon_info["price"]}\nseedless: {melon_info["seedless"]}\nflesh_color: {melon_info["flesh_color"]}\nrind color: {melon_info["rind_color"]}\naverage_weight: {melon_info["avg_weight"]}\n\n')
--- melons.py
+++ melons.py
@@ -1,23 +1,77 @@
+
 melon_names = {
     1: 'Honeydew',
     2: 'Crenshaw',
     3: 'Crane',
     4: 'Casaba',
-    5: 'Cantaloupe',
+    5: 'Cantaloupe'
 }
 
-melon_prices = {
-    1: 0.99,
-    2: 2.00,
-    3: 2.50,
-    4: 2.50,
-    5: 0.99,
+melons = {
+
+"Honeydew": {
+    "price": 0.99,
+    "seedless": True,
+    "flesh_color": None,
+    "rind_color": None,
+    "avg_weight": None
+},
+
+"Crenshaw": {
+    "price": 2.00,
+    "seedless": False,
+    "flesh_color": None,
+    "rind_color": None,
+    "avg_weight": None
+},
+
+"Crane": {
+    "price": 2.50,
+    "seedless": False,
+    "flesh_color": None,
+    "rind_color": None,
+    "avg_weight": None
+},
+
+"Casaba": {
+    "price": 2.50,
+    "seedless": False,
+    "flesh_color": None,
+    "rind_color": None,
+    "avg_weight": None
+},
+
+"Cantaloupe": {
+    "price": 0.99,
+    "seedless": False,
+    "flesh_color": None,
+    "rind_color": None,
+    "avg_weight": None
 }
 
-melon_seedlessness = {
-    1: True,
-    2: False,
-    3: False,
-    4: False,
-    5: False,
+
 }
+
+# "melon_names" = {
+#     1: 'Honeydew',
+#     2: 'Crenshaw',
+#     3: 'Crane',
+#     4: 'Casaba',
+#     5: 'Cantaloupe',
+# }
+
+# "prices" = {
+#     1: 0.99,
+#     2: 2.00,
+#     3: 2.50,
+#     4: 2.50,
+#     5: 0.99,
+# }
+
+# "seedless": {
+#     1: True,
+#     2: False,,
+#     3: False,,
+#     4: False,,
+#     5: False,,
+# }
